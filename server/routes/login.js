2910aaf979c9a25cac02c3b683b977814196468d
farukkandemir <farukkandemir09@gmail.com>
2024-06-16 15:28:38 -0600

    chore: Update bcrypt dependency to bcryptjs for password hashing

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 router.route("/").post(async (req, res) => {
