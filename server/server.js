1281ac8bbaf212762596a88c0ea3b5bd5c61f03b
farukkandemir <farukkandemir09@gmail.com>
2024-06-15 20:09:42 -0600

    chore: Update CORS configuration to allow requests from localhost:5173

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
